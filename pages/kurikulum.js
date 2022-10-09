b880f906f7eb5a208103d138d153ddb788fd58cb
Juethroo <denisjethro@gmail.com>
2022-10-09 21:03:23 +0700

    BiG Update Nopal

---
+++ pages/kurikulum.js
@@ -0,0 +1,31 @@
+// javascript navbar
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+//javarscript slider
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".image-slide");
+
+var sliderNav = function (manual) {
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    })
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    })
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
